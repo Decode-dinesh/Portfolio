32932d6a707e5c0aac64ec5e9cce37e8269a543f
Decode-dinesh <280997dinesh@gmail.com>
2021-09-24 14:03:10 +0530

    portfolio built

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Navigation from "./components/Navigation";
+import Greetings from "./containers/Greetings";
+import Skills from "./containers/Skills";
+import Proficiency from "./containers/Proficiency";
+import Education from "./containers/Education";
+
+import GithubProfile from "./containers/GithubProfile";
+
+
+import "./assets/vendor/nucleo/css/nucleo.css";
+import "./assets/vendor/font-awesome/css/font-awesome.min.css";
+import "./assets/css/argon-design-system-react.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<>
+		<Navigation />
+		<Greetings />
+		<Skills />
+		<Proficiency />
+		<Education />
+		<GithubProfile />
+	</>,
+	document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/portfolio.js
@@ -0,0 +1,139 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+	name: "Dinesh P",
+	title: "Hi all, I'm Dinesh",
+	description:
+		"A passionate Full Stack Web Developer having an experience of building Web applications with JavaScript / Reactjs / Nodejs and some other cool libraries and frameworks.",
+	resumeLink: "#",
+};
+
+export const openSource = {
+	githubUserName: "Decode-dinesh",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+	facebook: "#",
+	instagram: "https://www.instagram.com/#",
+	twitter: "https://twitter.com/280997?s=09",
+	github: "https://github.com/Decode-dinesh",
+	linkedin: "https://www.linkedin.com/in/dinesh-p-37a773210/",
+};
+
+export const skillsSection = {
+	title: "What I do",
+	subTitle:
+		"CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+	skills: [
+		emoji(
+			"⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+		),
+		emoji(
+			"⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"
+		),
+		emoji(
+			"⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+		),
+	],
+
+	softwareSkills: [
+		{
+			skillName: "html-5",
+			fontAwesomeClassname: "vscode-icons:file-type-html",
+		},
+		{
+			skillName: "css3",
+			fontAwesomeClassname: "vscode-icons:file-type-css",
+		},
+		{
+			skillName: "JavaScript",
+			fontAwesomeClassname: "logos:javascript",
+		},
+		{
+			skillName: "reactjs",
+			fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+		},
+		{
+			skillName: "nodejs",
+			fontAwesomeClassname: "logos:nodejs-icon",
+		},	
+		{
+			skillName: "npm",
+			fontAwesomeClassname: "vscode-icons:file-type-npm",
+		},
+		{
+			skillName: "sql-database",
+			fontAwesomeClassname: "vscode-icons:file-type-sql",
+		},
+		{
+			skillName: "mongoDB",
+			fontAwesomeClassname: "vscode-icons:file-type-mongo",
+		},
+		{
+			skillName: "aws",
+			fontAwesomeClassname: "logos:aws",
+		},
+		{
+			skillName: "python",
+			fontAwesomeClassname: "logos:python",
+		},
+		{
+			skillName: "git",
+			fontAwesomeClassname: "logos:git-icon",
+		},
+	
+	],
+};
+
+export const SkillBars = [
+	{
+		Stack: "Frontend/Design", //Insert stack or technology you have experience in
+		progressPercentage: "70", //Insert relative proficiency in percentage
+	},
+	{
+		Stack: "Backend",
+		progressPercentage: "60",
+	},
+	{
+		Stack: "Programming",
+		progressPercentage: "50",
+	},
+];
+
+export const educationInfo = [
+	{
+		schoolName: "SRM University",
+		subHeader: "Master of Science in Physics and nanotechnology",
+		duration: "JUNE 2019 - May 2021",
+		desc: "Project.",
+		descBullets: [
+			"Synthesis and characterization of Cobalt oxide/ nickel oxide",
+			"NanoComposite prepared by co-precipitation",
+		],
+	},
+	{
+		schoolName: "St. Joseph's college Arts and Science",
+		subHeader: "Bachelor of Science in Physics",
+		duration: "July 2015 -  May 2018",
+		desc: "Participated in the research of XXX and published 3 papers.",
+		descBullets: [
+			"Lorem ipsum dolor sit amet, consectetfgur adipiscing elit",
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		],
+	},
+	{
+		schoolName: "Avichi Higher Secondary School",
+		subHeader: "class - X  and class - XII",
+		duration: "June 2012 -  April 2015",
+		desc: "Biology group",
+		descBullets: [
+			"Lorem ipsum dolor sit amet, consectetfgur adipiscing elit",
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		],
+	},
+	
+
+];
+
